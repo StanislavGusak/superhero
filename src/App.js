2a162ae3143198ce4b66e8e4cdfe3d12aeb8e520
StanislavGusak <117658029+StanislavGusak@users.noreply.github.com>
2023-04-09 14:53:15 +0300

    add basename

--- src/App.js
+++ src/App.js
@@ -25,6 +25,6 @@ const App = () => {
       ]
     }
   ])
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} basename='/StanislavGusak' />;
 }
 export default App;
