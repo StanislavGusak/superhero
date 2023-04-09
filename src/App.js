ab75801686c7de81902412237555316c735f5909
StanislavGusak <117658029+StanislavGusak@users.noreply.github.com>
2023-04-09 16:40:21 +0300

    change basename

--- src/App.js
+++ src/App.js
@@ -25,6 +25,6 @@ const App = () => {
       ]
     }
   ])
-  return <RouterProvider router={router} basename='/stanislavgusak.github.io' />;
+  return <RouterProvider router={router} basename='/stanislavgusak' />;
 }
 export default App;
